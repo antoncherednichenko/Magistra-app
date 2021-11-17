1858f92e92daecfc5540f6ad0d3e3ac2fea3a997
antoncherednichenko <tony.cherednichenko95@yandex.ru>
2021-11-18 00:04:06 +0300

    finished marckup of math component

--- src/Components/Math/Math.jsx
+++ src/Components/Math/Math.jsx
@@ -2,31 +2,32 @@ import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Header from '../Main/Header/Header'
 import s from './math.module.css'
+import MathBush from './MathBush/MathBush'
 
 export default function Math(){
     const[cards, setCards] = useState([
         {
             id: 1,
             title: 'ЛАБИРИНТЫ',
-            icon: '#',
+            icon: './img/labirint.svg',
             route: '/maze'
         },
         {
             id: 2,
             title: 'ТАБЛИЦА ШУЛЬТЕ',
-            icon: '#',
+            icon: './img/table.svg',
             route: '/table_shulte'
         },
         {
             id: 3,
             title: 'РЕШИ ПРИМЕР',
-            icon: '#',
+            icon: './img/primer.svg',
             route: '/solve'
         },
         {
             id: 4,
             title: 'ЗАПОМНИ И ПОВТОРИ',
-            icon: '#',
+            icon: './img/memory.svg',
             route: '/remember'
         },
     ])
@@ -39,14 +40,15 @@ export default function Math(){
                         {cards.map(card => (
                             <Link className={s.card} to={card.route} key={card.id}>
                                 <h2 className={s.title}>{card.title}</h2>
+                                <img className={s.icon} src={card.icon} alt='icon' />
                             </Link>
                         ))}
-                        {/* <div className={s.square}></div>
-                        <div className={s.circle}></div>
-                        <div className={s.triangle}></div> */}
                     </div>
-                    
                 </div>
+                <MathBush bushPos={s.mathBush} />
+                <div className={s.square}></div>
+                <div className={s.circle}></div>
+                <div className={s.triangle}></div>
             </div>
         </>
     )
